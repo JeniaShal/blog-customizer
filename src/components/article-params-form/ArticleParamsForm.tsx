0929c78a7c11af2759f469b20ffe743b809f9789
Jenia Shalomeenko <evgeniyashalomeenko@yandex.ru>
2024-08-02 18:19:29 +0300

    added button functional and header

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,31 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from 'components/text';
+// import { Select } from 'components/select';
 
+import { useState } from 'react';
 import styles from './ArticleParamsForm.module.scss';
+// import  { titleParams } from '../../constants/articleParamsProps';
+// import { fontFamilyOptions } from '../../constants/articleProps';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const onClick = () => {
+		setIsOpen(!isOpen);
+	};
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} OnClick={onClick} />
+
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text as={'h2'} size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					{/* <Select></Select> */}
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
